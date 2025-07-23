a0a1b89054ae835e4c919acac8725996c4079361
Bruno <dev.bruno.ds@gmail.com>
2025-07-23 19:05:37 -0300

    .

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date'
+    ];
+
+    public function projects(){
+        $this->belongsTo(Project::class);
+    }
 }
--- database/migrations/2025_07_23_154557_create_tasks_table.php
+++ database/migrations/2025_07_23_154557_create_tasks_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
+            $table->enum('priority', ['low', 'medium', 'high'])->default('medium');
+            $table->date('due_date')->nullable();
             $table->timestamps();
         });
     }
